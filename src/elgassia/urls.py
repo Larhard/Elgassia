7529ef1381f03be07d8eb3639cf375357ebbceae
Larhard <larhard@gmail.com>
2014-09-25 15:03:13 +0200

    auto redirect to main page

--- src/elgassia/urls.py
+++ src/elgassia/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 import main.urls
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', 'main.views.home'),
                        url(r'^main/', include(main.urls, namespace='main')),
+                       url(r'^$', RedirectView.as_view(url=reverse_lazy('main:home'))),
                        )
