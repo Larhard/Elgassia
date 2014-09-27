dfec028b9f97d0df651dc79ece971dfc5c233c46
Larhard <larhard@gmail.com>
2014-09-27 19:37:57 +0200

    remove popup_menu model and dest_name field

--- main/model_generics.py
+++ main/model_generics.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class MenuGeneric(models.Model):
     title = models.CharField(max_length=256, default='')
-    dest_name = models.CharField(max_length=256, default='')
     dest_url = models.CharField(max_length=256, default='')
     dest_page = models.CharField(max_length=256, default='')
 
--- main/models.py
+++ main/models.py
@@ -6,10 +6,6 @@ class MainMenu(model_generics.MenuGeneric):
     pass
 
 
-class PopupMenu(model_generics.MenuGeneric):
-    pass
-
-
 class StandardPage(model_generics.PageGeneric):
     pass
 
