8b5077774c9eb6bfc917c09629e48d0ef58c9376
Larhard <larhard@gmail.com>
2014-09-27 19:40:46 +0200

    bugfix

--- main/templatetags/main/menus.py
+++ main/templatetags/main/menus.py
@@ -1,19 +1,8 @@
 from django import template
 
-from main.models import MainMenu, PopupMenu
+from main.models import MainMenu
 
 register = template.Library()
-
-
-class PopupMenuNode(template.Node):
-    def render(self, context):
-        context['popup_menu'] = PopupMenu.objects.order_by('position')
-        return ''
-
-
-@register.tag()
-def get_popup_menu(parser, token):
-    return PopupMenuNode()
 
 
 class MainMenuNode(template.Node):
